82ecf6f37211243fcdfc5d4fab351c4496790875
ekeyes694 <ekeyes693@gmail.com>
2016-04-14 19:40:41 -0400

    validated inputs

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 angular.module('myApp', [])
 .controller('oneController',function($scope){
+  $scope.hideText = true;
 
-})
+  $scope.submit = function(){
+    if ($scope.myForm.$valid){
+      $scope.hideText = false;
+    }
+  }
+
+  $scope.reset = function(){
+  location.reload();
+  }
+});
